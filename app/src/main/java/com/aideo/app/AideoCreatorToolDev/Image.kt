ce61a76eb4f299ba479b7f487ddaca6bb409c3de
s-suri <edurathore9999@gmail.com>
2024-05-08 11:22:10 +0530

    new changes

--- app/src/main/java/com/aideo/app/AideoCreatorToolDev/Image.kt
+++
@@ -1,3 +0,0 @@
-package com.android.myapplication
-
-class Image(var title: String, var path: String, var size: Long)
--- app/src/main/java/com/aideo/app/ImageEditor/filters/FilterListener.kt
+++
@@ -1,7 +0,0 @@
-package com.aideo.app.ImageEditor.filters
-
-import ja.burhanrashid52.photoeditor.PhotoFilter
-
-interface FilterListener {
-    fun onFilterSelected(photoFilter: PhotoFilter)
-}
